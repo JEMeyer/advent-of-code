f66ed474f84aa6703e16e6afec5e0de92a2d6e7a
Joe Meyer <joseph.meyer@paige.ai>
2022-12-02 11:23:31 -0600

    refactor solution

--- 2022/02/rps.py
+++ 2022/02/rps.py
@@ -6,61 +6,60 @@ Module Docstring
 __author__ = "Joe Meyer"
 __license__ = "MIT"
 
+move_to_points = {'A': 1, 'B': 2, 'C': 3}
 
-def play_rock_paper_scissors_pt1(games):
+
+def play_game(opponent, player):
+    # tie
+    if opponent == player:
+        return 3 + move_to_points[player]
+    # win
+    elif (opponent == 'A' and player == 'B') or (opponent == 'B' and player == 'C') or (opponent == 'C' and player == 'A'):
+        return 6 + move_to_points[player]
+    #loss
+    else:
+        return move_to_points[player]
+
+def pick_move(opponent, outcome):
+    if outcome == 'X': #loss
+        if opponent == 'A':
+            return 'C'
+        elif opponent == 'B':
+            return 'A'
+        else:
+            return 'B'
+    if outcome == 'Z': #win
+        if opponent == 'A':
+            return 'B'
+        elif opponent == 'B':
+            return 'C'
+        else:
+            return 'A'
+    else: # draw
+        return opponent
+
+def convert_player_move(move, opponent_move = ''):
+    if opponent_move == '': # straight mapping from XYZ to ABC
+        if move == 'X':
+            return 'A'
+        elif move == 'Y':
+            return 'B'
+        else:
+            return 'C'
+    else:
+        return pick_move(opponent_move, move)
+
+def solve_puzzle(games, pt2 = False):
     total_score = 0
     for game in games:
         plays = game.split(' ')
-        if plays[0] == 'A': # rock
-            if plays[1] == 'X': #rock
-                total_score = total_score + 4 # draw
-            if plays[1] == 'Y': # paper
-                total_score = total_score + 8 # win
-            if plays[1] == 'Z': # sciccors
-                total_score = total_score + 3 # loss
-        elif plays[0] == 'B': # paper
-            if plays[1] == 'X': #rock
-                total_score = total_score + 1 # loss
-            if plays[1] == 'Y': # paper
-                total_score = total_score + 5 # draw
-            if plays[1] == 'Z': # sciccors
-                total_score = total_score + 9 # win
-        else: # scissors
-            if plays[1] == 'X': #rock
-                total_score = total_score + 7 # win
-            if plays[1] == 'Y': # paper
-                total_score = total_score + 2 # loss
-            if plays[1] == 'Z': # sciccors
-                total_score = total_score + 6 # draw
-
-    return total_score
-
-
-def play_rock_paper_scissors_pt2(games):
-    total_score = 0
-    for game in games:
-        plays = game.split(' ')
-        if plays[0] == 'A': # rock
-            if plays[1] == 'X': # loss
-                total_score = total_score + 3
-            if plays[1] == 'Y': # draw
-                total_score = total_score + 4
-            if plays[1] == 'Z': # win
-                total_score = total_score + 8
-        elif plays[0] == 'B': # paper
-            if plays[1] == 'X': # loss
-                total_score = total_score + 1
-            if plays[1] == 'Y': # draw
-                total_score = total_score + 5
-            if plays[1] == 'Z': # win
-                total_score = total_score + 9
-        else: # scissors
-            if plays[1] == 'X': # loss
-                total_score = total_score + 2
-            if plays[1] == 'Y': # draw
-                total_score = total_score + 6
-            if plays[1] == 'Z': # win
-                total_score = total_score + 7
+        opponent_move = plays[0]
+        user_input = plays[1]
+        convert_move_second_param = opponent_move if pt2 else ''
+        total_score = total_score + play_game(
+            opponent_move,
+            convert_player_move(user_input, convert_move_second_param)
+        )
 
     return total_score
 
@@ -69,7 +68,7 @@ def main():
     """ Main entry point of the app """
     file_input = open('rps_input.txt', 'r')
     lines = file_input.read().splitlines()
-    score = play_rock_paper_scissors_pt2(lines)
+    score = solve_puzzle(lines, True)
     print(score)
 
 
