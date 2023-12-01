0d608664201f8612c3ea9ba4c946d694b2b3e824
Joe Meyer <joseph.meyer@paige.ai>
2023-12-01 11:16:24 -0600

    add .sln and day1 input

--- 2023/Day1.cs
+++ 2023/Day1.cs
@@ -1,8 +1,34 @@
+using System;
+using System.IO;
+
 public class Day1 : IDay
 {
-    public void Run()
-    {
-        // Your Day 1 code goes here
-        Console.WriteLine("Running Day 1...");
+    public void Run() {
+        string inputPath = "input/Day1.txt";
+        try {
+            int sum = 0;
+            // Read each line of the file
+            foreach (string line in File.ReadLines(inputPath)) {
+                sum += ProcessLine(line);
+            }
+            Console.WriteLine($"Sum of calibration values: {sum}");
+        }
+        catch (IOException e) {
+            Console.WriteLine($"An error occurred while reading the file: {e.Message}");
+        }
+    }
+
+    int ProcessLine(string line) {
+        string firstDigit, lastDigit = "";
+        for (int i=0; i< a.line; i++) {
+            if (Char.IsDigit(a[i])) {
+                b += a[i];
+            }
+        }
+        // Find first digit
+
+        // Find last digit
+
+        // Concat
     }
 }
